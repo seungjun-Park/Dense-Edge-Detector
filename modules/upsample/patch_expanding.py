9d7541b06f4c6f00ef823709c717549ed112a399
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2025-06-23 17:03:40 +0900

    modifeid patch expand

--- modules/upsample/patch_expanding.py
+++ modules/upsample/patch_expanding.py
@@ -13,17 +13,19 @@ class PatchExpanding(UpSample):
                  ):
         super().__init__(*args, **kwargs)
 
-        self.expand = nn.Conv2d(
-            self.in_channels,
-            (self.scale_factor ** 2) * self.out_channels,
-            kernel_size=1,
-            bias=False,
+        self.expand = nn.Sequential(
+            nn.Conv2d(
+                self.in_channels,
+                (self.scale_factor ** 2) * self.out_channels,
+                kernel_size=1,
+                bias=False,
+            ),
+            nn.PixelShuffle(int(self.scale_factor))
         )
 
         self.norm = LayerNorm(self.out_channels)
 
     def _forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.expand(x)
-        x = rearrange(x, 'b (c p1 p2) h w -> b c (p1 h) (p2 w)', p1=self.scale_factor, p2=self.scale_factor, c=self.out_channels)
 
         return self.norm(x)

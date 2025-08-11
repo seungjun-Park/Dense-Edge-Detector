4ffb72ac52711c450cb25119d3d7f0967d6b2cff
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2025-08-11 10:21:39 +0900

    modified granularity prediction.

--- modules/block/film.py
+++ modules/block/film.py
@@ -6,18 +6,14 @@ from modules.block import Block
 class FiLM(Block):
     def __init__(self,
                  in_channels: int,
-                 gamma_max: float = 0.5,
                  *args,
                  **kwargs
                  ):
         super().__init__(*args, **kwargs)
 
-        self.gamma_max = gamma_max
-
         self.gamma_beta = nn.Conv2d(in_channels, in_channels * 2, 1)
 
     def _forward(self, x: torch.Tensor, cond: torch.Tensor) -> torch.Tensor:
         gamma, beta = self.gamma_beta(cond).chunk(2, dim=1)
-        gamma = self.gamma_max * torch.tanh(gamma)
 
-        return x * (1 + gamma) + beta
+        return x * gamma + beta

52f5a807dc1badd1829b21b989286b791d77ec5e
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2025-10-24 20:37:09 +0900

    modified adapter.

--- modules/block/adapter.py
+++ modules/block/adapter.py
@@ -12,17 +12,38 @@ class Adapter(nn.Module):
 
         self.layers = nn.ModuleList()
 
+        # for i in range(num_blocks):
+        #     self.layers.append(
+        #         nn.Sequential(
+        #             nn.Conv2d(in_channels, int(in_channels * 4), kernel_size=1),
+        #             nn.ReLU(),
+        #             nn.Conv2d(int(in_channels * 4), in_channels, kernel_size=1),
+        #         )
+        #     )
+
         for i in range(num_blocks):
             self.layers.append(
                 nn.Sequential(
-                    nn.Conv2d(in_channels, int(in_channels * 4), kernel_size=1),
-                    nn.ReLU(),
-                    nn.Conv2d(int(in_channels * 4), in_channels, kernel_size=1),
+                    nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
+                    nn.GroupNorm(1, in_channels),
+                    nn.GELU(),
+                    nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1)
                 )
             )
+
+        # for i in range(num_blocks):
+        #     self.layers.append(
+        #         nn.Sequential(
+        #             nn.Conv2d(in_channels, in_channels, kernel_size=7, padding=3, groups=in_channels),
+        #             nn.GroupNorm(1, in_channels),
+        #             nn.Conv2d(in_channels, int(in_channels * 4), kernel_size=1),
+        #             nn.GELU(),
+        #             nn.Conv2d(int(in_channels * 4), in_channels, kernel_size=1,)
+        #         )
+        #     )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for layer in self.layers:
             x = layer(x) + x
-        
+
         return x

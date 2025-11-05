e0165cd9a34782f8a83f69b3c63d4276c77401c6
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2025-11-06 00:26:58 +0900

    modified adapter

--- modules/block/adapter.py
+++ modules/block/adapter.py
@@ -1,7 +1,25 @@
 import torch
 import torch.nn as nn
+from typing import List
 
 from thirdparty.convnext_v2 import LayerNorm, GRN
+
+
+class Permute(torch.nn.Module):
+    """This module returns a view of the tensor input with its dimensions permuted.
+
+    Args:
+        dims (List[int]): The desired ordering of dimensions
+    """
+
+    def __init__(self, dims: List[int]):
+        super().__init__()
+        self.dims = dims
+
+    def forward(self, x: torch.CharTensor) -> torch.Tensor:
+        return torch.permute(x, self.dims)
+
+
 
 class Adapter(nn.Module):
     def __init__(self,
@@ -20,11 +38,13 @@ class Adapter(nn.Module):
 
         self.layer = nn.Sequential(
             nn.Conv2d(in_channels, in_channels, kernel_size=7, stride=1, padding=3, groups=in_channels),
-            LayerNorm(in_channels, eps=1e-6, data_format='channels_first'),
-            nn.Conv2d(in_channels, int(in_channels * mlp_ratio), kernel_size=1),
+            Permute([0, 2, 3, 1]),
+            LayerNorm(in_channels, eps=1e-6),
+            nn.Linear(in_channels, int(in_channels * mlp_ratio)),
             nn.GELU(),
             GRN(int(in_channels * mlp_ratio)),
-            nn.Conv2d(int(in_channels * mlp_ratio), in_channels, kernel_size=1),
+            nn.Linear(int(in_channels * mlp_ratio), in_channels),
+            Permute([0, 3, 1, 2])
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:

6b6592f1b1d4413f5684e90c8253977ce1b2c8d9
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2025-11-05 23:33:09 +0900

    modified adapter.

--- modules/block/adapter.py
+++ modules/block/adapter.py
@@ -1,37 +1,31 @@
 import torch
 import torch.nn as nn
 
-from thirdparty.convnext_v2 import LayerNorm
+from thirdparty.convnext_v2 import LayerNorm, GRN
 
 class Adapter(nn.Module):
     def __init__(self,
                  in_channels: int,
                  net_type: str = 'vgg',
-                 num_blocks: int = 1,
                  *args,
                  **kwargs,
                  ):
         super().__init__(*args, **kwargs)
 
-        self.layers = nn.ModuleList()
         net_type = net_type.lower()
         assert net_type in ['vgg', 'convnext']
 
         self.last_activation = nn.ReLU() if net_type == 'vgg' else nn.GELU()
         mlp_ratio = 4 if net_type == 'vgg' else 2
 
-        for i in range(num_blocks):
-            self.layers.append(
-                nn.Sequential(
-                    LayerNorm(in_channels, data_format='channels_first'),
-                    nn.Conv2d(in_channels, int(in_channels * mlp_ratio), kernel_size=1),
-                    nn.GELU(),
-                    nn.Conv2d(int(in_channels * mlp_ratio), in_channels, kernel_size=1),
-                )
-            )
+        self.layer = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=7, stride=1, padding=3, groups=in_channels),
+            LayerNorm(in_channels, eps=1e-6),
+            nn.Conv2d(in_channels, int(in_channels * mlp_ratio), kernel_size=1),
+            nn.GELU(),
+            GRN(int(in_channels * mlp_ratio)),
+            nn.Conv2d(int(in_channels * mlp_ratio), in_channels, kernel_size=1),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        for layer in self.layers:
-            x = layer(x) + x
-
-        return self.last_activation(x)
+        return self.last_activation(self.layer(x))
